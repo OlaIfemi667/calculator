1ba80953fcc537248d46547158d61a003d003790
Ola <bancolejeandedieu@gmail.com>
2024-11-05 11:56:22 +0100

    feat: styles and basics operations

--- javascript.js
+++ javascript.js
@@ -1,17 +1,80 @@
 const NUMBERS = document.querySelectorAll(".number");
-let firstVar = "";
-let secondVar = ""
+const OPERATORS = document.querySelectorAll(".operation");
+const egality = document.querySelector("#equalTo")
+let result = 0;
+let firstVar = null;
+let secondVar = null;
+let display = "";
+let currentOperator = "";
+
+function inputOperand() {
+    NUMBERS.forEach(element => {
+        element.addEventListener("click", () => {
+            if (display.length <= 9) {
+                display += element.value;
+                textBrowser.textContent = display;
+            }
+        });
+    });
+}
+function clear(){
+    result = 0;
+    firstVar = null;
+    secondVar = null;
+    display = "";
+    currentOperator = "";
+    textBrowser.textContent = result
+}
+function inputOperator() {
+    OPERATORS.forEach(element => {
+        element.addEventListener("click", () => {
+            if (firstVar === null) {
+                firstVar = Number(display);
+            } else if (display !== "") {
+                secondVar = Number(display);
+                result = operate(currentOperator, firstVar, secondVar);
+                textBrowser.textContent = result;
+                firstVar = result;
+            }
+            display = "";
+            currentOperator = element.value;
+            
+        });
+    });
+    egality.addEventListener("click", () => {
+        if (firstVar === null) {
+            firstVar = Number(display);
+        }
+        else if (display !== "") {
+            secondVar = Number(display);
+            result = operate(currentOperator, firstVar, secondVar);
+            textBrowser.textContent = result;
+            firstVar = result;
+        }
+        display = "";
+    })
+}
+function operate(operator, a, b){
+    switch (operator){
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "x":
+            return a * b;
+        case "÷":
+            return a / b;
+        default:
+            return "ERROR"
+    }
+    
+}
 function main(){
     textBrowser = document.querySelector("#textBrowser");
-    NUMBERS.forEach(element => {
-        element.addEventListener("click", () =>{
-            firstVar += element.value;
-            console.log(element.value)
-            textBrowser.textContent = firstVar;
-
-        })
-        console.log("inputs onclick set")
-    });
+    textBrowser.textContent = `${result}`;
+    inputOperand();
+    inputOperator();
+    document.querySelector("#cleanScreen").addEventListener("click", clear);
 }
 
 main();
